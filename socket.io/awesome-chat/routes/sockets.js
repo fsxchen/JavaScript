1f0c0619f54849895a4e490c1d4d37dd3c544b92
fsxchen <fsxchen@gmail.com>
2016-01-18 15:00:56 +0800

    socket.io

--- socket.io/awesome-chat/routes/sockets.js
+++ socket.io/awesome-chat/routes/sockets.js
@@ -1,18 +1,73 @@
+// var io = require('socket.io');
+//
+// exports.initialize = function(server) {
+//   io = io.listen(server);
+//   io.sockets.on("connection", function(socket){
+//     socket.on('message', function(message){
+//       message= JSON.parse(message);
+//       if(message.type == "userMessage"){
+//         // socket.get('nickname', function(err, nickname){
+//           message.username=socket.nickname;
+//           socket.broadcast.send(JSON.stringify(message));
+//           message.type = "myMessage";
+//           socket.send(JSON.stringify(message));
+//         // });
+//       }
+//     });
+//     socket.on("set_name", function(data){
+//         socket.nickname = data.name
+//         socket.emit('name_set', data);
+//         socket.send(JSON.stringify({type:'serverMessage',
+//             message: 'Welcome to the most interesting chat room on earth!'}));
+//         socket.broadcast.emit('user_entered', data);  //广播有用户进来了
+//     });
+//   });
+// }
+
+
 var io = require('socket.io');
 
-exports.initialize = function(server) {
+exports.initialize = function (server) {
   io = io.listen(server);
-  io.sockets.on("connection", function(socket){
-    socket.send(JSON.stringify(
-      {type:'serverMessage',
-        message: 'Welcome to the most interesting chat room on earth!'}));
-    socket.on('message', function(message){
-      message= JSON.parse(message);
-      if(message.type == "userMessage"){
-        socket.broadcast.send(JSON.stringify(message));
-        message.type = "myMessage";
-        socket.send(JSON.stringify(message));
-      }
+  var self = this
+  var user = {nickname:""}
+
+  this.chatInfra = io.of("/chat_infra");
+  this.chatInfra.on("connection", function (socket) {
+      socket.on("set_name", function (data) {
+        user.nickname = data.name,
+        socket.emit('name_set', data);
+        socket.send(JSON.stringify({type:'serverMessage',
+            message:'Welcome to the most interesting ' +
+            'chat room on earth!'}));
+        socket.broadcast.emit('user_entered', data);
+      });
+      socket.on("join_room", function (room) {
+        nickname = user.nickname
+        socket.join(room.name);
+        var comSocket = self.chatCom.sockets[socket.id];
+        comSocket.join(room.name);
+        comSocket.room = room.name;
+        socket.in(room.name).broadcast
+    .emit('user_entered', {'name':nickname});
+
     });
-  });
-};
+});
+
+
+  this.chatCom = io.of("/chat_com");
+    this.chatCom.on("connection", function (socket) {
+        socket.on('message', function (message) {
+          message = JSON.parse(message);
+          if (message.type == "userMessage") {
+              console.log(socket.nickname)
+              console.log(".....");
+              message.username = user.nickname;
+              socket.in(socket.room).broadcast.send(JSON.stringify(message));
+              socket.broadcast.send(JSON.stringify(message));
+              message.type = "myMessage";
+              socket.send(JSON.stringify(message));
+          }
+        });
+      });
+}
